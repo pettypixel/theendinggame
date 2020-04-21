31f5fafefc5b7367e138467e7944f79012643eab
Michel G. Ågren <michel@feverish.club>
2020-04-21 13:58:24 +0200

    Reformat Options.js

--- frontend/src/Components/Buttons/Options.js
+++ frontend/src/Components/Buttons/Options.js
@@ -4,56 +4,56 @@ import '../../Styles/button.css'
 
 class Options extends Component {
 
-/*firstAnswer = [];
-secondAnswer = [];
-lastAnswer = []; */
+  /*firstAnswer = [];
+  secondAnswer = [];
+  lastAnswer = []; */
 
-constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-      firstAnswer : [ 
+      firstAnswer: [
         { "Yes": 1 },
-        { "No": 0} 
-     ],
-      secondAnswer : [
+        { "No": 0 }
+      ],
+      secondAnswer: [
         { "Yes": 1 },
-        { "No" : 0 }
-    ],
-      lastAnswer : [
+        { "No": 0 }
+      ],
+      lastAnswer: [
         { "Yes": 1 },
-        { "No" : 0 }
-    ]
- }
+        { "No": 0 }
+      ]
+    }
     this.handleEvent = this.handleEvent.bind(this);
-}
+  }
 
-handleEvent(){
+  handleEvent() {
 
-}
+  }
 
-handleAnswer(answer){
-    if ( this.firstAnswer.value === 0 ){
-        console.log("You've clicked Yes");
-    } else{
-        console.log("Go to question 2");
+  handleAnswer(answer) {
+    if (this.firstAnswer.value === 0) {
+      console.log("You've clicked Yes");
+    } else {
+      console.log("Go to question 2");
     }
 
-    if( this.secondAnswer.value === 0 ){
-        console.log("You've clicked Yes");
-    } else{
-        console.log("Go to question 3");
+    if (this.secondAnswer.value === 0) {
+      console.log("You've clicked Yes");
+    } else {
+      console.log("Go to question 3");
     }
 
     console.log(answer);
-}
+  }
 
-render () { 
-    return  <div>
+  render() {
+    return <div>
         <h2> Hi and Welcome to this test and find out if you're allowed to go outside</h2>            
             <Button className="answer"> YES I WANT TO CONTINUE </Button>
             <Button className="answer"> NO I WANT TO STAY </Button>
         </div>
-    }
+  }
 }
 
 export default Options;
